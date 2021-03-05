66729149b154c8172121f2573f5415764efdf687
earrrice <w10999999999@gmail.com>
2021-03-05 16:01:57 +0800

    ad

---
+++ out.py
@@ -0,0 +1,6 @@
+import sys
+from x4 import ff
+
+x=int(sys.argv[1])
+
+print(x4.x1(x))
---
+++ pump.py
@@ -0,0 +1,6 @@
+import sys
+import random
+
+studentid=["109021111","109021222","109021333"]
+
+print(random.choice(studentid))
